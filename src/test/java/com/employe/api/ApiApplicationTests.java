87b408ca2983b0295592fc8e249b82bf4bf09ce2
pdramirez-dev <pdramirez.h@outlook.com>
2022-10-20 01:19:22 -0400

    Api Employed Services firts Commit

---
+++ src/test/java/com/employe/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employe.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
